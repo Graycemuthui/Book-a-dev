ea0e48d0a4cec048bd668a1c225798e7b424768d
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-11-07 16:18:02 +0100

    Add file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+} from 'react-router-dom';
+import React from 'react';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
-function App() {
-  return <h1>Hello World!</h1>;
-}
+const App = () => {
 
-ReactDOM.render(<App />, document.getElementById("root"));
+  return (
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/developers" element={<Outlet />}>
+              <Route index element={<Developers />} />
+              <Route path=":id" element={<Outlet />}>
+                <Route index element={<DeveloperDetails />} />
+              </Route>
+            </Route>
+            <Route path="/developers/new" element={<CreateDeveloper />} />
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/bookings/new" element={<CreateBooking />} />
+            <Route path="*" element={<Developers />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ app/javascript/pages/CreateDeveloper.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateDeveloper = () => {
+  return (
+    <div>CreateDeveloper</div>
+  )
+}
+
+export default CreateDeveloper
